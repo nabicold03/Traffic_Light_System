ec93bdcb6558cf5b54b7110c1e3cddf6cd5acab1
nabicold03 <binh.vunabi2509@hcmut.edu.vn>
2024-05-03 10:03:41 +0700

    add more states for Manual Mode to adjust valur for each light_time, the lowest values are 3,2 and 1 for red, green and yellow respectively

--- Traffic_Light_System/Traffic_Light_System.srcs/sources_1/new/Traffic_Light_System.sv
+++ Traffic_Light_System/Traffic_Light_System.srcs/sources_1/new/Traffic_Light_System.sv
@@ -24,7 +24,7 @@ typedef struct packed{
     reg [6:0] max_light_time;
     reg [6:0] current_light_time;
 } struct_t;
-typedef enum reg [2:0] {START=0, RED_GREEN = 1, RED_YELLOW = 2, GREEN_RED = 3, YELLOW_RED = 4, MANUAL = 5} state;
+typedef enum reg [2:0] {START=0, RED_GREEN = 1, RED_YELLOW = 2, GREEN_RED = 3, YELLOW_RED = 4, MANUAL_RED = 5, MANUAL_GREEN = 6, MANUAL_YELLOW = 7} state;
 
 module Traffic_Light_System(
     input wire clk,rst,changemode,inc,dec,setup,
@@ -41,6 +41,16 @@ module Traffic_Light_System(
         col.current_light_time <= col.current_light_time - 1;
     endfunction
     
+    function automatic void decrementmanual(ref struct_t col);
+        col.current_light_time <= col.current_light_time - 1;
+        col.max_light_time <= col.max_light_time - 1;
+    endfunction
+    
+    function automatic void incrementmanual(ref struct_t col);
+        col.current_light_time <= col.current_light_time + 1;
+        col.max_light_time <= col.max_light_time + 1;
+    endfunction
+    
     function automatic void setuptime(ref struct_t col, input logic [6:0] num);
         col.current_light_time <= num;
         col.max_light_time <= num;
@@ -54,31 +64,84 @@ module Traffic_Light_System(
         next_state=START;
         case(current_state)
             START: begin
-                next_state=RED_GREEN;
-            end
-            
-            RED_GREEN: begin
-                if(green.current_light_time <= 0) next_state=RED_YELLOW;
+                if(changemode) next_state=MANUAL_RED;
                 else next_state=RED_GREEN;
             end
             
+            RED_GREEN: begin
+                if(changemode) begin
+                    next_state=MANUAL_RED;
+                end
+                else begin
+                    if(green.current_light_time <= 0) next_state=RED_YELLOW;
+                    else next_state=RED_GREEN;
+                end
+            end
+            
             RED_YELLOW: begin
-                if(yellow.current_light_time <= 0) next_state=GREEN_RED;
-                else next_state=RED_YELLOW;
+                if(changemode) begin
+                    next_state=MANUAL_RED;
+                end
+                else begin
+                    if(yellow.current_light_time <= 0) next_state=GREEN_RED;
+                    else next_state=RED_YELLOW;
+                end
             end
             
             GREEN_RED: begin
-                if(green.current_light_time <= 0) next_state=YELLOW_RED;
-                else next_state=GREEN_RED;
+                if(changemode) begin
+                    next_state=MANUAL_RED;
+                end
+                else begin
+                    if(green.current_light_time <= 0) next_state=YELLOW_RED;
+                    else next_state=GREEN_RED;
+                end
             end
             
             YELLOW_RED: begin
-                if(yellow.current_light_time <= 0) next_state=RED_GREEN;
-                else next_state=YELLOW_RED;
+                if(changemode) begin
+                    next_state=MANUAL_RED;
+                end
+                else begin
+                    if(yellow.current_light_time <= 0) next_state=RED_GREEN;
+                    else next_state=YELLOW_RED;
+                end
             end
             
-            MANUAL: begin
+            MANUAL_RED: begin
+                if(changemode) begin
+                    next_state=MANUAL_GREEN;
+                end
+                else begin
+                    if(setup) begin
+                        next_state=RED_GREEN;
+                    end
+                    else next_state=MANUAL_RED;
+                end
+            end
             
+            MANUAL_GREEN: begin
+                if(changemode) begin
+                    next_state=MANUAL_YELLOW;
+                end
+                else begin
+                    if(setup) begin
+                        next_state=RED_GREEN;
+                    end
+                    else next_state=MANUAL_GREEN;
+                end
+            end
+            
+            MANUAL_YELLOW: begin
+                if(changemode) begin
+                    next_state=MANUAL_RED;
+                end
+                else begin
+                    if(setup) begin
+                        next_state=RED_GREEN;
+                    end
+                    else next_state=MANUAL_YELLOW;
+                end
             end
             
             default: next_state = START;
@@ -100,6 +163,11 @@ module Traffic_Light_System(
                 setuptime(red,5);
                 setuptime(green,3);
                 setuptime(yellow,2);
+                if(changemode) begin
+                    setuptime(red,3);
+                    setuptime(green,2);
+                    setuptime(yellow,1);
+                end
             end
             
             RED_GREEN: begin
@@ -117,6 +185,11 @@ module Traffic_Light_System(
                     decrement(green);
                 end
                 decrement(red);
+                if(changemode) begin
+                    setuptime(red,3);
+                    setuptime(green,2);
+                    setuptime(yellow,1);
+                end
             end
             
             RED_YELLOW: begin
@@ -138,6 +211,11 @@ module Traffic_Light_System(
                     decrement(yellow);
                     decrement(red);
                 end
+                if(changemode) begin
+                    setuptime(red,3);
+                    setuptime(green,2);
+                    setuptime(yellow,1);
+                end
             end
             
             GREEN_RED: begin
@@ -155,6 +233,11 @@ module Traffic_Light_System(
                     decrement(green);
                 end
                 decrement(red);
+                if(changemode) begin
+                    setuptime(red,3);
+                    setuptime(green,2);
+                    setuptime(yellow,1);
+                end
             end
             
             YELLOW_RED: begin
@@ -176,10 +259,38 @@ module Traffic_Light_System(
                     decrement(yellow);
                     decrement(red);
                 end
+                if(changemode) begin
+                    setuptime(red,3);
+                    setuptime(green,2);
+                    setuptime(yellow,1);
+                end
             end
             
-            MANUAL: begin
+            MANUAL_RED: begin
+                if(dec) begin
+                    decrementmanual(red);
+                end
+                else if(inc) begin
+                    incrementmanual(red);
+                end
+            end
             
+            MANUAL_GREEN: begin
+                if(dec) begin
+                    decrementmanual(green);
+                end
+                else if(inc) begin
+                    incrementmanual(green);
+                end
+            end
+            
+            MANUAL_YELLOW: begin
+                if(dec) begin
+                    decrementmanual(yellow);
+                end
+                else if(inc) begin
+                    incrementmanual(yellow);
+                end
             end
             
             default: begin
